9d4da279a10cac0ef1ee71a413f566dc9d47f7a8
Gennadii <atlantagenafes@gmail.com>
2022-09-14 13:33:15 +0300

    minAgeRestriction refactor 12

--- src/routes/video-routes.ts
+++ src/routes/video-routes.ts
@@ -1,6 +1,6 @@
 import {Request, Response, Router} from "express";
 
-function isIsoDate(str) {
+function isIsoDate(str: any) {
     if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(str)) return false;
     const d = new Date(str);
     return d.toISOString() === str; // valid date
